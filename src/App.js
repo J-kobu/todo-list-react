20a77bcf10e00430c2a6ff4eeb69c23d65198714
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2022-10-19 23:20:53 -0300

    Image deleted

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import freecodecamplogo from './images/freecodecamp-logo.svg';
 import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="Todo-List-App">
-      <div className='freecodecamp-logo-container'>
-        <img src={freecodecamplogo} className='freecodecamp-logo' alt='free code camp logo' />
-      </div>
       <div className='main-todo-list'>
         <h1>My todo list</h1>
         <TodoList />
